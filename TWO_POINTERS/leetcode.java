541ebe731fb756376384b9515724b13ba6fef45a
tanya0404 <149752404+tanya0404@users.noreply.github.com>
2025-11-15 12:50:41 +0530

    first commit

---
+++ TWO_POINTERS/leetcode.java
@@ -0,0 +1,32 @@
+88. Merge Sorted Array
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i=m-1;
+        int j=n-1;
+        int[] newnums=new int[m+n];
+        int k=newnums.length-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                newnums[k--]=nums1[i--];
+            }
+            else  newnums[k--]=nums2[j--];
+            
+        }
+        while(i>=0){
+            newnums[k--]=nums1[i--];
+        }
+        while(j>=0){
+            newnums[k--]=nums2[j--];
+        }
+         for (int x = 0; x < m+n; x++) {
+            nums1[x] = newnums[x];
+        }
+    }
+}
+
+
+
+
+
+
